a544eeb8dac55d5230955f851e0d34ee3354b064
Oluwatobi Awe <oluwatobi.awe@live.com>
2019-04-11 14:51:21 +0100

    Addition of Web Project

--- RentalPortal.Order/Startup.cs
+++ RentalPortal.Order/Startup.cs
@@ -45,7 +45,6 @@ namespace RentalPortal.Order
 
             services.AddMemoryCache();
             services.AddAutoMapper();
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
